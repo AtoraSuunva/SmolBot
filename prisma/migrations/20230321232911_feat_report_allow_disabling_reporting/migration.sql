d01b12479c812383cdef6918539f4ef96edd9518
AtoraSuunva <atora@giraffeduck.com>
2023-03-22 06:19:46 -0400

    chore: optimize caching in dockerfile, move prisma deploy to dockerfile, fix migrate file

--- prisma/migrations/20230321232911_feat_report_allow_disabling_reporting/migration.sql
+++ prisma/migrations/20230321232911_feat_report_allow_disabling_reporting/migration.sql
@@ -1,14 +1,4 @@
--- RedefineTables
-PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_ReportConfig" (
-    "guild_id" TEXT NOT NULL PRIMARY KEY,
-    "enabled" BOOLEAN NOT NULL DEFAULT false,
-    "channel_id" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "updated_at" DATETIME NOT NULL
-);
-INSERT INTO "new_ReportConfig" ("channel_id", "guild_id", "message", "updated_at") SELECT "channel_id", "guild_id", "message", "updated_at" FROM "ReportConfig";
-DROP TABLE "ReportConfig";
-ALTER TABLE "new_ReportConfig" RENAME TO "ReportConfig";
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+-- Manually written, we can migrate from the previous table just using this
+-- The prisma migration doesn't go "DEFAULT FALSE" since the schema doesn't do that
+ALTER TABLE "ReportConfig"
+  ADD COLUMN "enabled" BOOLEAN NOT NULL DEFAULT FALSE;
