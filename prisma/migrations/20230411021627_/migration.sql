4584fc4e0f3aef6d6d7831ef822a395c7ff7b3c4
AtoraSuunva <at@atora.dev>
2023-04-10 23:48:07 -0400

    fix(migrations): allow migration to work

--- prisma/migrations/20230411021627_/migration.sql
+++ prisma/migrations/20230411021627_/migration.sql
@@ -1,34 +1,28 @@
-/*
-  Warnings:
-
-  - Added the required column `channel_update` to the `ModLogConfig` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_ModLogConfig" (
     "guild_id" TEXT NOT NULL PRIMARY KEY,
     "channel_id" TEXT NOT NULL,
     "updated_at" DATETIME NOT NULL,
-    "enabled" BOOLEAN NOT NULL,
-    "member_add" BOOLEAN NOT NULL,
-    "member_add_new" INTEGER NOT NULL,
-    "member_add_invite" BOOLEAN NOT NULL,
-    "member_welcome" BOOLEAN NOT NULL,
-    "member_remove" BOOLEAN NOT NULL,
-    "member_remove_roles" BOOLEAN NOT NULL,
-    "member_ban" BOOLEAN NOT NULL,
-    "member_unban" BOOLEAN NOT NULL,
-    "user_update" TEXT NOT NULL,
-    "message_delete" BOOLEAN NOT NULL,
-    "message_delete_bulk" BOOLEAN NOT NULL,
-    "channel_create" BOOLEAN NOT NULL,
-    "channel_delete" BOOLEAN NOT NULL,
-    "channel_update" BOOLEAN NOT NULL,
-    "reaction_actions" BOOLEAN NOT NULL,
-    "automod_action" BOOLEAN NOT NULL
+    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "member_add" BOOLEAN NOT NULL DEFAULT false,
+    "member_add_new" INTEGER NOT NULL DEFAULT 0,
+    "member_add_invite" BOOLEAN NOT NULL DEFAULT false,
+    "member_welcome" BOOLEAN NOT NULL DEFAULT false,
+    "member_remove" BOOLEAN NOT NULL DEFAULT false,
+    "member_remove_roles" BOOLEAN NOT NULL DEFAULT false,
+    "member_ban" BOOLEAN NOT NULL DEFAULT false,
+    "member_unban" BOOLEAN NOT NULL DEFAULT false,
+    "user_update" TEXT NOT NULL DEFAULT 'None',
+    "message_delete" BOOLEAN NOT NULL DEFAULT false,
+    "message_delete_bulk" BOOLEAN NOT NULL DEFAULT false,
+    "channel_create" BOOLEAN NOT NULL DEFAULT false,
+    "channel_delete" BOOLEAN NOT NULL DEFAULT false,
+    "channel_update" BOOLEAN NOT NULL DEFAULT false,
+    "reaction_actions" BOOLEAN NOT NULL DEFAULT false,
+    "automod_action" BOOLEAN NOT NULL DEFAULT false
 );
-INSERT INTO "new_ModLogConfig" ("automod_action", "channel_create", "channel_delete", "channel_id", "enabled", "guild_id", "member_add", "member_add_invite", "member_add_new", "member_ban", "member_remove", "member_remove_roles", "member_unban", "member_welcome", "message_delete", "message_delete_bulk", "reaction_actions", "updated_at", "user_update") SELECT "automod_action", "channel_create", "channel_delete", "channel_id", "enabled", "guild_id", "member_add", "member_add_invite", "member_add_new", "member_ban", "member_remove", "member_remove_roles", "member_unban", "member_welcome", "message_delete", "message_delete_bulk", "reaction_actions", "updated_at", "user_update" FROM "ModLogConfig";
+INSERT INTO "new_ModLogConfig" ("automod_action", "channel_create", "channel_delete", "channel_id", "channel_update", "enabled", "guild_id", "member_add", "member_add_invite", "member_add_new", "member_ban", "member_remove", "member_remove_roles", "member_unban", "member_welcome", "message_delete", "message_delete_bulk", "reaction_actions", "updated_at", "user_update") SELECT "automod_action", "channel_create", "channel_delete", "channel_id", "channel_update", "enabled", "guild_id", "member_add", "member_add_invite", "member_add_new", "member_ban", "member_remove", "member_remove_roles", "member_unban", "member_welcome", "message_delete", "message_delete_bulk", "reaction_actions", "updated_at", "user_update" FROM "ModLogConfig";
 DROP TABLE "ModLogConfig";
 ALTER TABLE "new_ModLogConfig" RENAME TO "ModLogConfig";
 PRAGMA foreign_key_check;
