7b3ceedd28e7935e32b8adce8998cdf2858f3670
AtoraSuunva <atora@giraffeduck.com>
2023-06-28 23:01:19 +0900

    feat(logging): instrument the bot with actual logging

--- src/modules.ts
+++ src/modules.ts
@@ -3,8 +3,10 @@ import { secretModules } from './secret/index.js'
 import { miscModules } from './misc/index.js'
 import { modModules } from './mod/index.js'
 import { SleetModule } from 'sleetcord'
+import { logging } from './logging.js'
 
 export const modules: SleetModule[] = [
+  logging,
   ...modModules,
   ...miscModules,
   ...utilityModules,
