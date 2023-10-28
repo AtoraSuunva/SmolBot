b73606d9268be1f5032338e27c882e30ad9931a2
AtoraSuunva <atora@giraffeduck.com>
2023-10-28 05:10:10 -0400

    fix(antiraid): missing dm permission and default member permissions

--- src/mod/antiraid/index.ts
+++ src/mod/antiraid/index.ts
@@ -6,6 +6,8 @@ const antiraid = new SleetSlashCommand({
   name: 'antiraid',
   description: 'Configure the antiraid module',
   options: [antiraid_config, antiraid_check],
+  dm_permission: false,
+  default_member_permissions: ['ManageGuild'],
 })
 
 export const antiraidModules = [antiraid]
